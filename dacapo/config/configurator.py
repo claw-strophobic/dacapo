cda2377a084a8e1358a2f627eab889e83c546ccb
Claw Strophobic <Claw.Strophobic@gmx.de>
2016-03-05 13:23:06 +0100

    Set text- and backgroundcolour of preview

--- dacapo/config/configurator.py
+++ dacapo/config/configurator.py
@@ -23,6 +23,10 @@ class MyFontChooserWidget(Gtk.FontChooserWidget):
 
 	def __init__(self):
 		super(MyFontChooserWidget, self).__init__()
+		provider = Gtk.CssProvider()
+		provider.load_from_data('.entry { background: white; }')
+		Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(), provider,
+			Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
 		# a font chooser
 		# self.font_chooser = Gtk.FontChooserWidget()
@@ -33,11 +37,16 @@ class MyFontChooserWidget(Gtk.FontChooserWidget):
 		#	"This is an example of preview text!")
 		self.set_show_preview_entry(True)
 		self.grid = None
+		self.entry = None
 		for attr in self:
 			self.grid = attr
 			break
 		if self.grid <> None:
-			pass
+			for attr in self.grid:
+				if type(attr).__name__ == "SearchEntry":
+					self.searchEntry = attr
+				if type(attr).__name__ == "Entry":
+					self.entry = attr
 
 		## print(dir(Gtk.FontChooserWidget))
 		box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
@@ -50,7 +59,7 @@ class MyFontChooserWidget(Gtk.FontChooserWidget):
 
 		# connect signal from the font chooser to the callback function
 		self.connect("notify::font", self.font_cb)
-		self.printValues()
+		# self.printValues()
 
 		# add the font chooser to the window
 		# self.add(self.font_chooser)
@@ -60,11 +69,37 @@ class MyFontChooserWidget(Gtk.FontChooserWidget):
 	def font_cb(self, event, user_data):
 		# print in the terminal
 		print("You chose the font " + self.get_font())
+		return
+
+	def set_font(self, fontName, fontSize):
+		super(MyFontChooserWidget, self).set_font('{!s} {!s}'.format(fontName, fontSize))
+		self.searchEntry.set_text(fontName)
+		return
+
+	def setBGcolor(self, type):
+		g = CONFIG.gui[type]
+		print('Background-Color: {!s}'.format(g.getRGBABackgroundColor().to_string()))
+		self.entry.override_background_color(Gtk.StateFlags.NORMAL, g.getRGBABackgroundColor())
+		return
+
+	def setFGcolor(self, field):
+		self.colorchooser.set_rgba(field.font.getRGBAColor())
+		self.entry.override_color(Gtk.StateFlags.NORMAL, field.font.getRGBAColor())
+		self.colorchooser.set_property("show-editor", True)
+		return
+
 
 	def printValues(self):
 		print("Los gehts...")
 		for attr in self:
-			print('Field: {!s}'.format(attr))
+			print('Field: {!s}'.format(attr.__dict__))
+			grid = attr
+			break
+		for attr in grid:
+			print('Field: {!s}'.format(type(attr).__name__))
+			print('Field: {!s}'.format(type(attr).__dict__))
+		return
+
 
 
 class Configurator(Gtk.Window):
@@ -147,12 +182,11 @@ class Configurator(Gtk.Window):
 		print("You chose the field " + fieldName)
 		field = model.get_value(combo_iter, 1)
 		font_chooser = combo.font_chooser
+		font_chooser.setBGcolor(combo.type)
 		font = '{!s} {!s}'.format(field.font.fontName, field.font.fontSize)
 		print("Field-Font " + font + " fontColor: " + field.font.getRGBAColor().to_string())
-		font_chooser.set_font(font)
-		font_chooser.colorchooser.set_rgba(field.font.getRGBAColor())
-		font_chooser.colorchooser.set_property("show-editor", True)
-
+		font_chooser.set_font(field.font.fontName, field.font.fontSize)
+		font_chooser.setFGcolor(field)
 
 
 win = Configurator()
