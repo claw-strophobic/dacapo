be018afb1b8ccfc494cbdebfbfb996ac237824d9
Claw Strophobic <Claw.Strophobic@gmx.de>
2016-07-05 20:19:41 +0200

    Add first part of position to config gui

--- dacapo/config/gui/tabs.py
+++ dacapo/config/gui/tabs.py
@@ -180,11 +180,11 @@ class FieldTab(PreviewTab):
 		grid = Gtk.Grid()
 		grid.set_column_homogeneous(False)
 		grid.set_column_spacing(6)
-		self.font_chooser = MyFontChooserWidget()
 		self.notebook = Gtk.Notebook()
 
-		self.fields = self.get_field_combo(type, self.font_chooser)
+		self.fields = get_field_combo(type)
 		self.fields.set_tooltip_text(_("Select an existing field here."))
+		self.fields.connect("changed", self.on_field_combo_changed)
 		labelField = Gtk.Label(_('Field:'))
 		grid.add(labelField)
 		grid.attach_next_to(self.fields, labelField, Gtk.PositionType.RIGHT, 1, 1)
@@ -203,25 +203,10 @@ class FieldTab(PreviewTab):
 		vbox.add(grid)
 		page_font = FieldFontTab(self)
 		self.notebook.append_page(page_font, Gtk.Label(_("Font settings")))
-		page_pos = FieldPosTab(self)
-		self.notebook.append_page(page_pos, Gtk.Label(_("Position settings")))
+		self.page_pos = FieldPosTab(self)
+		self.notebook.append_page(self.page_pos, Gtk.Label(_("Position settings")))
 		vbox.add(self.notebook)
 		self.add(vbox)
-
-	def get_field_combo(self, type, font_chooser):
-		type_store = Gtk.ListStore(str, object)
-		g = CONFIG.gui[type]
-		for key in g.fields:
-			type_store.append([key, g.fields[key]])
-		field_combo = Gtk.ComboBox.new_with_model(type_store)
-		field_combo.type = type
-		field_combo.font_chooser = font_chooser
-		renderer = Gtk.CellRendererText()
-		field_combo.pack_start(renderer, True)
-		field_combo.add_attribute(renderer, 'text', 0)
-		field_combo.connect("changed", self.on_field_combo_changed)
-		field_combo.set_entry_text_column(0)
-		return field_combo
 
 	def on_field_combo_changed(self, combo):
 		combo_iter = combo.get_active_iter()
@@ -231,17 +216,16 @@ class FieldTab(PreviewTab):
 		fieldName = model.get_value(combo_iter, 0)
 		field = model.get_value(combo_iter, 1)
 		self.field = field
-		font_chooser = combo.font_chooser
-		font_chooser.setBGcolor(combo.type)
-		fontFace = '{!s} {!s}'.format(field.font.fontWeight, field.font.fontStyle)
-		## print(u"Field-Font " + font + " fontColor: " + field.font.getRGBAColor().to_string())
-		font_chooser.set_font(field.font.name, field.font.fontWeight, field.font.fontStyle, field.font.fontSize)
-		# font_chooser.set_font(field.font.name, fontFace, field.font.fontSize)
-		font_chooser.setFGcolor(field.font.getRGBAColor())
 		audio = CONFIG.getConfig('TEMP', Key='AUDIOFILE')
 		self.apply_button.set_sensitive(True)
 		if audio is not None:
 			self.prev_button.set_sensitive(True)
+		i = self.notebook.get_n_pages()
+		for x in range(0, i):
+			p = self.notebook.get_nth_page(x)
+			print self.notebook.get_tab_label_text(p)
+			p.fillFields()
+
 
 	def getBlitObject( self ):
 		audio = CONFIG.getConfig('TEMP', Key='AUDIOFILE')
@@ -259,35 +243,50 @@ class FieldTab(PreviewTab):
 			print self.notebook.get_tab_label_text(p)
 			p.apply()
 
-
-class FieldFontTab(Gtk.Box):
-
+class FieldChildTab(Gtk.Box):
 	def __init__(self, fieldTab):
-		super(FieldFontTab, self).__init__()
+		super(FieldChildTab, self).__init__()
 		self.fieldTab = fieldTab
 		self.set_border_width(10)
-		vbox = Gtk.VBox()
+		self.vbox = Gtk.VBox()
 		self.set_border_width(10)
-		grid = Gtk.Grid()
-		grid.set_column_homogeneous(False)
-		grid.set_column_spacing(6)
+		self.grid = Gtk.Grid()
+		self.grid.set_column_homogeneous(False)
+		self.grid.set_column_spacing(6)
+
+
+class FieldFontTab(FieldChildTab):
+
+	def __init__(self, fieldTab):
+		super(FieldFontTab, self).__init__(fieldTab)
 
 		## add fields
-		self.fieldTab.font_chooser.colorchooser.connect_color_activated(self.onColorChanged)
+		self.font_chooser = MyFontChooserWidget()
+		self.font_chooser.colorchooser.connect_color_activated(self.onColorChanged)
 
-		vbox.add(grid)
-		vbox.add(self.fieldTab.font_chooser)
-		self.add(vbox)
+		self.vbox.add(self.grid)
+		self.vbox.add(self.font_chooser)
+		self.add(self.vbox)
+
+	def fillFields(self):
+		font_chooser = self.font_chooser
+		font = self.fieldTab.field.font
+		font_chooser.setBGcolor(self.fieldTab.guiType)
+		fontFace = '{!s} {!s}'.format(font.fontWeight, font.fontStyle)
+		## print(u"Field-Font " + font + " fontColor: " + field.font.getRGBAColor().to_string())
+		font_chooser.set_font(font.name, font.fontWeight, font.fontStyle, font.fontSize)
+		# font_chooser.set_font(field.font.name, fontFace, field.font.fontSize)
+		font_chooser.setFGcolor(font.getRGBAColor())
 
 	def onColorChanged(self, obj, colorchooser):
 		self.color = colorchooser.get_rgba()
 
 
 	def apply(self):
-		color = self.fieldTab.font_chooser.colorchooser.get_rgba()
+		color = self.font_chooser.colorchooser.get_rgba()
 		self.fieldTab.field.font.setValue('fontColor', color)
 
-		pangoFontDesc = self.fieldTab.font_chooser.get_font_desc()
+		pangoFontDesc = self.font_chooser.get_font_desc()
 		if pangoFontDesc is not None:
 			font = pangoFontDesc.get_family()
 			size = pangoFontDesc.get_size() / 1024
@@ -299,47 +298,119 @@ class FieldFontTab(Gtk.Box):
 			self.fieldTab.field.font.setValue('fontWeight', weight.value_nick)
 			self.fieldTab.field.font.setValue('fontStyle', style.value_nick)
 
-class FieldPosTab(Gtk.Box):
+class FieldPosTab(FieldChildTab):
 
 	def __init__(self, fieldTab):
-		super(FieldPosTab, self).__init__()
-		self.fieldTab = fieldTab
-		self.set_border_width(10)
-		vbox = Gtk.VBox()
-		self.set_border_width(10)
-		grid = Gtk.Grid()
-		grid.set_column_homogeneous(False)
-		grid.set_column_spacing(6)
+		super(FieldPosTab, self).__init__(fieldTab)
 		g = CONFIG.gui[self.fieldTab.guiType]
 
+		labelPosH = Gtk.Label(_('The horizontal position'))
+		labelPosV = Gtk.Label(_('The vertical position'))
+		labelRefH = Gtk.Label(_('The horizontal reference position'))
+		labelRefV = Gtk.Label(_('The vertical reference position'))
 		labelWidth = Gtk.Label(_('Maximal field width'))
 		labelHeight = Gtk.Label(_('Maximal field height'))
 		labelDummy = Gtk.Label(' ')
 
-		grid.add(labelWidth)
-		adjustment = Gtk.Adjustment(int(g.width), 0, int(g.width), 1, 10, 0)
+		# Horizontal Position
+		self.grid.add(labelPosH)
+		self.posH_spinbutton = Gtk.SpinButton()
+		self.posH_spinbutton.set_tooltip_text(_("Set here the horizontal position of the field."))
+		self.grid.attach_next_to(self.posH_spinbutton, labelPosH, Gtk.PositionType.RIGHT, 1, 1)
+
+		self.grid.attach_next_to(labelRefH, self.posH_spinbutton, Gtk.PositionType.RIGHT, 1, 1)
+		self.comboRefHFields = get_field_combo(self.fieldTab.guiType)
+		self.comboRefHFields.set_tooltip_text(_("Select an existing field here."))
+		self.grid.attach_next_to(self.comboRefHFields, labelRefH, Gtk.PositionType.RIGHT, 1, 1)
+
+		# Vertical Position
+		self.grid.attach_next_to(labelPosV, labelPosH, Gtk.PositionType.BOTTOM, 1, 1)
+		self.posV_spinbutton = Gtk.SpinButton()
+		self.posV_spinbutton.set_tooltip_text(_("Set here the vertical position of the field."))
+		self.grid.attach_next_to(self.posV_spinbutton, labelPosV, Gtk.PositionType.RIGHT, 1, 1)
+
+		self.grid.attach_next_to(labelRefV, self.posV_spinbutton, Gtk.PositionType.RIGHT, 1, 1)
+		self.comboRefVFields = get_field_combo(self.fieldTab.guiType)
+		self.comboRefVFields.set_tooltip_text(_("Select an existing field here."))
+		self.grid.attach_next_to(self.comboRefVFields, labelRefV, Gtk.PositionType.RIGHT, 1, 1)
+
+
+		# Max width and height
+		self.grid.attach_next_to(labelWidth, labelPosV, Gtk.PositionType.BOTTOM, 1, 1)
 		self.width_spinbutton = Gtk.SpinButton()
-		self.width_spinbutton.set_adjustment(adjustment)
-		self.width_spinbutton.set_value(int(g.width))
-		self.width_spinbutton.set_tooltip_text(_("Set here the pixel width of the display.") + " (max {!s})".format(g.width))
-		grid.attach_next_to(self.width_spinbutton,labelWidth, Gtk.PositionType.RIGHT, 1, 1)
+		self.width_spinbutton.set_tooltip_text(_("Set here the maximal pixel width of the field. Leave 0 for no max.") + " (max {!s})".format(g.width))
+		self.grid.attach_next_to(self.width_spinbutton,labelWidth, Gtk.PositionType.RIGHT, 1, 1)
 
-		grid.attach_next_to(labelHeight, labelWidth, Gtk.PositionType.BOTTOM, 1, 1)
-		adjustment = Gtk.Adjustment(int(g.height), 0, int(g.height), 1, 10, 0)
+		self.grid.attach_next_to(labelHeight, labelWidth, Gtk.PositionType.BOTTOM, 1, 1)
 		self.height_spinbutton = Gtk.SpinButton()
-		self.height_spinbutton.set_adjustment(adjustment)
-		self.height_spinbutton.set_value(int(g.height))
-		self.height_spinbutton.set_tooltip_text(_("Set here the pixel height of the display.") + " (max {!s})".format(g.height))
-		grid.attach_next_to(self.height_spinbutton,labelHeight, Gtk.PositionType.RIGHT, 1, 1)
+		self.height_spinbutton.set_tooltip_text(_("Set here the maximal pixel height of the field. Leave 0 for no max.") + " (max {!s})".format(g.height))
+		self.grid.attach_next_to(self.height_spinbutton,labelHeight, Gtk.PositionType.RIGHT, 1, 1)
 
-		vbox.add(grid)
-		self.add(vbox)
+		self.vbox.add(self.grid)
+		self.add(self.vbox)
+
+	def fillFields(self):
+		g = CONFIG.gui[self.fieldTab.guiType]
+		fieldpos = self.fieldTab.field.pos
+		adjustment = Gtk.Adjustment(fieldpos.posH, 0, int(g.width), 1, 10, 0)
+		self.posH_spinbutton.set_adjustment(adjustment)
+		self.posH_spinbutton.set_value(fieldpos.posH)
+
+		adjustment = Gtk.Adjustment(fieldpos.posV, 0, int(g.width), 1, 10, 0)
+		self.posV_spinbutton.set_adjustment(adjustment)
+		self.posV_spinbutton.set_value(fieldpos.posV)
+
+		adjustment = Gtk.Adjustment(fieldpos.maxWidth, 0, int(g.width), 1, 10, 0)
+		self.width_spinbutton.set_adjustment(adjustment)
+		self.width_spinbutton.set_value(fieldpos.maxWidth)
+
+		adjustment = Gtk.Adjustment(fieldpos.maxHeight, 0, int(g.width), 1, 10, 0)
+		self.height_spinbutton.set_adjustment(adjustment)
+		self.height_spinbutton.set_value(fieldpos.maxHeight)
+		model = self.comboRefHFields.get_model()
+		i = 0
+		self.comboRefHFields.set_active(0)
+		if fieldpos.posRefH is not None:
+			for row in model:
+				if row[0] == fieldpos.posRefH:
+					self.comboRefHFields.set_active(i)
+					break
+				i += 1
+
+		model = self.comboRefVFields.get_model()
+		self.comboRefVFields.set_active(0)
+		i = 0
+		if fieldpos.posRefV is not None:
+			for row in model:
+				if row[0] == fieldpos.posRefV:
+					self.comboRefVFields.set_active(i)
+					break
+				i += 1
 
 	def apply(self):
+		fieldpos = self.fieldTab.field.pos
+		posH = self.posH_spinbutton.get_value()
+		posV = self.posV_spinbutton.get_value()
+		fieldpos.setValue('posH', posH)
+		fieldpos.setValue('posV', posV)
+
+		model = self.comboRefHFields.get_model()
+		combo_iter = self.comboRefHFields.get_active_iter()
+		if combo_iter is not None:
+			fieldName = model.get_value(combo_iter, 0)
+			posRefH = fieldName
+			fieldpos.setValue('posRefH', posRefH)
+		model = self.comboRefVFields.get_model()
+		combo_iter = self.comboRefVFields.get_active_iter()
+		if combo_iter is not None:
+			fieldName = model.get_value(combo_iter, 0)
+			posRefV = fieldName
+			fieldpos.setValue('posRefV', posRefV)
+		print("posRefH: {!s} posRefV: {!s}".format(posRefH, posRefV))
 		maxW = self.width_spinbutton.get_value()
 		maxH = self.height_spinbutton.get_value()
-		self.fieldTab.field.pos.setValue('maxWidth', maxW)
-		self.fieldTab.field.pos.setValue('maxHeight', maxH)
+		fieldpos.setValue('maxWidth', maxW)
+		fieldpos.setValue('maxHeight', maxH)
 
 
 class GuiTab(Gtk.Box):
@@ -358,4 +429,19 @@ class GuiTab(Gtk.Box):
 		# self.notebook.append_page(self.page_font, Gtk.Label(_("Lyric-Font")))
 		self.page_fields = FieldTab(guiType)
 		self.notebook.append_page(self.page_fields, Gtk.Label(_("Fields")))
+
+
+def get_field_combo(type):
+	type_store = Gtk.ListStore(str, object)
+	g = CONFIG.gui[type]
+	type_store.append(['', None])
+	for key in g.fields:
+		type_store.append([key, g.fields[key]])
+	field_combo = Gtk.ComboBox.new_with_model(type_store)
+	field_combo.type = type
+	renderer = Gtk.CellRendererText()
+	field_combo.pack_start(renderer, True)
+	field_combo.add_attribute(renderer, 'text', 0)
+	field_combo.set_entry_text_column(0)
+	return field_combo
 
