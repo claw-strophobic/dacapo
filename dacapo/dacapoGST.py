252b58df0b6311a026e93ccd32f16a3b4c9b76e7
Claw Strophobic <claw.strophobic@gmx.de>
2017-03-13 15:04:27 +0100

    Get gapless work again

--- dacapo/dacapoGST.py
+++ dacapo/dacapoGST.py
@@ -99,67 +99,52 @@ class GstPlayer(threading.Thread):
 	def __init_pipelineGapless(self):
 		if self.debug: logging.debug("start creating gapless pipeline! ")
 		bReplayGain = self.config.getConfig('audio_engine', 'audio_engine', 'replayGain')
-		USE_QUEUE = True
-
-		# Pipeline erstellen
-		self.pipe = []
-		# self.pipe += Gst.Pipeline("player")
 		sinkType = self.config.getConfig('audio_engine', ' ', 'sinkType') + 'sink'
-		if self.debug: logging.debug("Versuche Sink: %s " % sinkType)
-		self.pipe, self.name = self.GStreamerSink(sinkType)
-		# self.pipe, self.name = self.GStreamerSink("alsasink")
-		conv = Gst.ElementFactory.make('audioconvert')
-		self.pipe = [conv] + self.pipe
-		prefix = []
+		if self.debug: logging.debug("Trying to create a sink of type: %s " % sinkType)
 
-		if USE_QUEUE:
-			queue = Gst.ElementFactory.make('queue')
-			queue.set_property('max-size-time', 500 * Gst.MSECOND)
-			prefix.append(queue)
+		# Create an empty Pipeline
+		data_pipeline = Gst.Pipeline()
 
-		# playbin2 has started to control the volume through pulseaudio,
-		# which means the volume property can change without us noticing.
-		# Use our own volume element for now until this works with PA.
-		# Also, when using the queue, this removes the delay..
-		self._vol_element = Gst.ElementFactory.make('volume')
-		prefix.append(self._vol_element)
-
-		# ReplayGain erstellen und zufügen
+		# Create the elements
+		data_convert = Gst.ElementFactory.make('audioconvert', "convert")
+		data_queue = Gst.ElementFactory.make('queue')
+		data_queue.set_property('max-size-time', 500 * Gst.MSECOND)
+		data_volume = Gst.ElementFactory.make('volume', "volume")
+		data_sink = Gst.ElementFactory.make(sinkType, "sink")
+		data_elements = [data_queue, data_volume]
+		# ReplayGain?
 		if bReplayGain:
-			if self.debug: logging.debug("ReplayGain wird aktiviert! ")
-			self.replay = Gst.ElementFactory.make("rgvolume", "replay")
-			prefix.append(self.replay)
+			if self.debug: logging.debug("activating ReplayGain! ")
+			data_rgain = Gst.ElementFactory.make("rgvolume", "replay")
+			data_elements.append(data_rgain)
+		data_elements.append(data_convert)
+		data_elements.append(data_sink)
 
-		self.pipe = prefix + self.pipe
-		# --------------------------------------------------------------------------------------------#
-		bufbin = Gst.Bin()
-		map(bufbin.add, self.pipe)
-		if len(self.pipe) > 1:
-			try:
-				Gst.element_link_many(*self.pipe)
-			except Gst.LinkError, e:
-				logging.error("Could not link GStreamer pipeline: '%s' " % e)
-				exc_type, exc_value, exc_traceback = sys.exc_info()
-				lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
-				for line in lines:
-					logging.error(line)
-				self.__destroy_pipeline()
-				return False
-
-		# Test to ensure output pipeline can preroll
-		bufbin.set_state(Gst.State.READY)
-		result, state, pending = bufbin.get_state(timeout=Gst.SECOND / 2)
-		if result == Gst.State.CHANGE_FAILURE:
-			bufbin.set_state(Gst.State.NULL)
-			self.__destroy_pipeline()
+		if (not data_pipeline or not data_convert or not data_sink):
+			logging.error("Could not create GStreamer pipeline. ")
 			return False
 
-		# Make the sink of the first element the sink of the bin
-		gpad = Gst.GhostPad.new('sink', self.pipe[0].get_pad('sink'))
-		bufbin.add_pad(gpad)
+		# Create a bin to put all together
+		# --------------------------------------------------------------------------------------------#
+		bufbin = Gst.Bin()
+		map(bufbin.add, data_elements)
+		if len(data_elements) > 1:
+			data_queue.link(data_volume)
+			if bReplayGain:
+				data_volume.link(data_rgain)
+				data_rgain.link(data_convert)
+			else:
+				data_volume.link(data_convert)
+			data_convert.link(data_sink)
+
 		# --------------------------------------------------------------------------------------------#
 
 
+		# Make the sink of the first element the sink of the bin
+		gpad = Gst.GhostPad.new('sink', data_elements[0].get_static_pad('sink'))
+		data_pipeline.add_pad(gpad)
+
+		logging.debug("Trying to create playbin. ")
 		self.player = Gst.ElementFactory.make("playbin", "player")
 		# by default playbin will render video -> suppress using fakesink
 		fakesink = Gst.ElementFactory.make("fakesink", "fakesink")
@@ -180,6 +165,7 @@ class GstPlayer(threading.Thread):
 		# --------------------------------------------------------------------------------------------#
 
 		# Hier wird die Ausgabe von playbin2 auf bufbin gesetzt!
+		logging.debug("Connecting the audi-sink from playbin with our bufbin. ")
 		self.player.set_property('audio-sink', bufbin)
 
 		bus = self.player.get_bus()
@@ -189,53 +175,6 @@ class GstPlayer(threading.Thread):
 		return
 
 	# --------------------------------------------------------------------------------------------#
-
-	def GStreamerSink(self, pipeline):
-		"""Try to create a GStreamer pipeline:
-		* Try making the pipeline (defaulting to gconfaudiosink or
-		  autoaudiosink on Windows).
-		* If it fails, fall back to autoaudiosink.
-		* If that fails, return None
-
-		Returns the pipeline's description and a list of disconnected elements."""
-
-		if not pipeline and not Gst.element_factory_find('gconfaudiosink'):
-			pipeline = "autoaudiosink"
-		elif not pipeline or pipeline == "gconf":
-			pipeline = "gconfaudiosink profile=music"
-
-		try:
-			pipe = [Gst.parse_launch(element) for element in pipeline.split('!')]
-		except GObject.GError, err:
-			logging.warning("Invalid GStreamer output pipeline, trying default. ")
-			try:
-				pipe = [Gst.parse_launch("autoaudiosink")]
-			except GObject.GError:
-				pipe = None
-			else:
-				pipeline = "autoaudiosink"
-
-		if pipe:
-			# In case the last element is linkable with a fakesink
-			# it is not an audiosink, so we append the default pipeline
-			fake = Gst.ElementFactory.make('fakesink')
-			try:
-				pipe[-1].link(fake)
-			except Gst.LinkError:
-				pass
-			else:
-				Gst.element_unlink_many(pipe[-1], fake)
-				default, default_text = pyGst.GStreamerSink("")
-				if default:
-					return pipe + default, pipeline + " ! " + default_text
-		else:
-			logging.error("Could not create default GStreamer pipeline. ")
-
-		return pipe, pipeline
-
-		# --------------------------------------------------------------------------------------------#
-
-
 
 		# create a simple function that is run when decodebin gives us the signal to let us
 		# know it got audio data for us. Use the get_pad call on the previously
@@ -325,9 +264,10 @@ class GstPlayer(threading.Thread):
 			elif message.type == Gst.MessageType.ELEMENT:
 				if self.debug: logging.debug("--> bin in on_message mit message.type %s " % t)
 				name = ""
-				if hasattr(message.structure, "get_name"):
-					name = message.structure.get_name()
+				if hasattr(message.get_structure(), "get_name"):
+					name = message.get_structure().get_name()
 					self.actualTitel = name
+					if self.debug: logging.debug("--> setting current title: %s" % name)
 
 				# This gets sent on song change. Because it is not in the docs
 				# we can not rely on it. Additionally we check in get_position
