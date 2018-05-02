b89708665470c7d6d9347e191e7c839c478ff986
Claw Strophobic <claw.strophobic@gmx.de>
2018-05-02 15:34:27 +0200

    Write songs to tempfile and open as playlist if we have more than one song
    If we write the song list to a tempfile, we can use the playlist
    feature of dacapo and shuffle the list.

--- dacapo/ql-plugins/openDacapo.py
+++ dacapo/ql-plugins/openDacapo.py
@@ -7,7 +7,7 @@
 # it under the terms of the GNU General Public License version 2 as
 # published by the Free Software Foundation
 #
-
+import tempfile
 from gi.repository import Gtk
 from quodlibet import qltk, app
 from quodlibet.qltk.wlw import WaitLoadWindow
@@ -23,13 +23,40 @@ class ConfirmAction(qltk.Message):
 	"""A message dialog that asks a yes/no question."""
 
 	def __init__(self, *args, **kwargs):
+		self.plugin = None
+		if kwargs.has_key("parent"):
+			self.parent = kwargs["parent"]
+			del kwargs["parent"]
 		kwargs["buttons"] = Gtk.ButtonsType.YES_NO
 		super(ConfirmAction, self).__init__(
 			Gtk.MessageType.WARNING, *args, **kwargs)
 
+		area = self.get_content_area()
+		area.set_border_width(10)
+		vbox = Gtk.VBox()
+		self.grid = Gtk.Grid()
+		self.grid.set_column_homogeneous(False)
+		self.grid.set_column_spacing(6)
+		self.grid.set_row_spacing(10)
+		vbox.set_spacing(10)
+
+		labelShuffle = Gtk.Label(_("Shuffle"), xalign=0)
+		self.grid.add(labelShuffle)
+
+		self.ShuffleSwitch = Gtk.Switch(halign=Gtk.Align.START, valign=Gtk.Align.CENTER)
+		self.ShuffleSwitch.set_tooltip_text(_("Use shuffled playback?"))
+		self.ShuffleSwitch.set_active(True)
+		self.grid.attach_next_to(self.ShuffleSwitch, labelShuffle, Gtk.PositionType.RIGHT, 1, 1)
+
+		vbox.add(self.grid)
+		area.add(vbox)
+		self.show_all()
+
 	def run(self, destroy=True):
 		"""Returns True if yes was clicked, False otherwise."""
 		resp = super(qltk.Message, self).run()
+		if self.parent != None:
+			self.parent.shuffle = self.ShuffleSwitch.get_active()
 		if destroy:
 			self.destroy()
 		if resp == Gtk.ResponseType.YES:
@@ -61,23 +88,36 @@ class OpenDacapo(SongsMenuPlugin):
 		if (songs is None) or (len(songs) <= 0):
 			return True
 
+		self.shuffle = False
 		if (len(songs) > 1) and not ConfirmAction(self.plugin_window,
 			_(self.PLUGIN_NAME),
-			_("Open {!s} files in dacapo?").format(len(songs))
-								  ).run():
+			_("Open {!s} files in dacapo?").format(len(songs)),
+			parent=self
+		).run():
 			return True
 
 		win = WaitLoadWindow(
 			self.plugin_window, len(songs),
 			_("Checking...\n\n%(current)d/%(total)d Songs done."))
 		args = []
-		for song in songs:
-			if song.get("~filename", "").endswith(".flac"):
-				args.append(song.get("~filename", ""))
-			if win.step():
-				break
+		if self.shuffle:
+			args.append("-s")
+		if (len(songs) > 1):
+			args.append("-pl")
+			f = tempfile.NamedTemporaryFile(delete=False)
+			for song in songs:
+				if song.get("~filename", "").endswith(".flac"):
+					f.writelines(song.get("~filename", "")+"\n")
+			f.close()
+			args.append(f.name)
+		else:
+			for song in songs:
+				if song.get("~filename", "").endswith(".flac"):
+					args.append(song.get("~filename", ""))
+
 		try:
 			program = 'dacapo'
+			print program,args
 			subprocess.call([program]+args)
 		except:
 			win.destroy()
